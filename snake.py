29c85f4f85f7dc12f895e41dd6e8b8e96222b98d
Ayo2837 <34068594+Ayo2837@users.noreply.github.com>
2017-12-01 12:07:05 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
-'''
+''
 Snake game.
 Authors:
-<insert the names of your teammates here>
+Ayo, Dejah, Maya
 '''
 
 import pygame
@@ -28,9 +28,9 @@ DIRECTION_UP = (0, -1)
 DIRECTION_DOWN = (0, 1)
 
 # Background color of the snake grid.
-COLOR_BACKGROUND = (255, 255, 255)  # rgb color for white
+COLOR_BACKGROUND = (135,206,250)  # rgb color for white
 # This is the color of the snake's head. 
-COLOR_SNAKE_HEAD = (255, 0, 0)      # rgb color for red
+COLOR_SNAKE_HEAD = (0,0,205)      # rgb color for red
 # This is the color of the rest of the snake.
 COLOR_SNAKE = (0, 255, 0)           # rgb color for green
 # This is the color for the snake's food.
@@ -48,6 +48,10 @@ def get_direction(previous_direction, event_key):
         return DIRECTION_LEFT
     elif event_key == pygame.K_UP:
         return DIRECTION_UP
+    elif event_key == pygame.K_DOWN:
+        return DIRECTION_DOWN
+    elif event_key == pygame.K_RIGHT:
+        return DIRECTION_RIGHT
     return previous_direction
 
 def create_food_position():
@@ -55,7 +59,12 @@ def create_food_position():
     The first element is the x position. Must be an int between 0 and GRID_WIDTH - 1, inclusively.
     The second element is the y position. Must be an int between 0 and GRID_HEIGHT - 1, inclusively.
     """
-    return
+    GRID_HEIGHT = 30
+    GRID_WIDTH = 30
+    tuple_lst = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+    return tuple_lst
+    
+
 
 def snake_ate_food(snake, food):
     """Returns whether food was eaten by the snake.
@@ -63,14 +72,27 @@ def snake_ate_food(snake, food):
     food - 2-tuple representing the position in the grid of the food
     This function should return True if the head of the snake is in the same position as food.
     """
+    if snake[0] == food:
+       return True
     return False
+
+
 
 def snake_ran_out_of_bounds(snake):
     """Returns whether the snake has ran off one of the four edges of the grid.
     snake - list of 2-tuples representing the positions of each snake segment
     Note that the grid is GRID_WIDTH cells wide and GRID_HEIGHT cells high.
     """
+    if snake[0][0] < 0:
+        return True
+    if snake[0][0] > 29:
+        return True
+    if snake[1][1] < 0:
+        return True
+    if snake[1][1] > 29:
+        return True
     return False
+
 
 def snake_intersected_body(snake):
     """Returns whether the snake has ran into itself.
@@ -78,8 +100,11 @@ def snake_intersected_body(snake):
     The snake ran into itself if the position of the head is the same as the position
     of any of its body segments.
     """
+    for each in range(1, len(snake)):
+        if snake[each] == snake[0]:
+            return True
     return False
-
+       
 def get_score(snake):
     """Returns the current score of the game.
     snake - list of 2-tuples representing the positions of each snake segment
@@ -193,6 +218,8 @@ def process_events(direction, game_over):
     for event in pygame.event.get():
         # Quit the program when the user presses the x in the corner of the window.
         if event.type == pygame.QUIT:
+            pygame.display.quit()
+            pygame.quit()
             sys.exit()
         # Process events when the user presses a key on the keyboard.
         # https://www.pygame.org/docs/ref/key.html
@@ -257,3 +284,6 @@ def start_game():
 
 # Start the snake game.
 start_game()
+
+
+    
